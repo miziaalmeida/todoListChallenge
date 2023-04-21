c43bbe699f8fa882d2175037f5665c14e80b59e5
mizialima <mizia.alima@gmail.com>
2023-04-21 15:54:10 -0300

    add project

---
+++ todoList/todoList/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  todoList
+//
+//  Created by Mizia Lima on 21/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
